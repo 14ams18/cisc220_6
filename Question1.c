64558a086c45da6b0bf23d810950fc84f4b3d031
14ams18 <abigael.schonewille@hotmail.com>
2016-12-07 18:24:19 -0500

    No compiler errors

--- Question1.c
+++ Question1.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<pthread.h>
+#include<unistd.h>
 
 #undef RAND_MAX
 #define RAND_MAX 100
@@ -10,21 +11,24 @@
 int prog[5] = {0, 0, 0, 0, 0};
 
 void *new_thread (void *threadid) {
-        int lane = (int)threadid -1
+        int lane = (int)threadid -1;
         do {
                 if (lane == 0) {  //draw thread
                         printf("Welcome to the CISC220 Racing Arena! /nHit Enter to move.");
-                        char past = "~";
-                        char togo = " ";
                         for (int i=0; i < 5; i++) {
-                                printf("%~*c", prog[i] , past);
+                                for (int j=0; j<prog[i]; j++){
+                                        putchar('~');
+                                }
                                 printf("|->");
                                 int invprog = 40 - prog[i];
-                                printf("%~*c", invprog , togo);
+                                for (int k=0; k<invprog; k++){
+                                        putchar(' ');
+                                }
                                 printf("# Lane %d #\n", i+1);
                         }
-                        usleep(20);
-                        system(clear);
+                        sleep(20);
+                        system("clear");
+                }
                 else if (lane == 1) { // Player thead
                         char move = getchar();
                         if (move == '\n') {
@@ -33,14 +37,15 @@ void *new_thread (void *threadid) {
                 }
                 else { //start the AI threads lane == 2,3,4,5
                         prog[lane-1] += 1;
-                        int sleep = rand();
-                        usleep(sleep);
+                        int srand = rand();
+                        sleep(srand);
                 }
         } while (prog[0] != 40 || prog[1] != 40 || prog[2] != 40 || prog[3] != 40 || prog[4] != 40);
         if (lane == 0) { //draw thread
                 for (int i=0; i < 5; i++) {
                         if (prog[i] == 40) {
-                                printf("Player %d WINS", i+1);
+                                printf("Player %d WINS\n", i+1);
+
                         }
                 }
         }
@@ -48,14 +53,14 @@ void *new_thread (void *threadid) {
 }
 
 int main(){
-        pthread threads[NUM_THREADS];
+        pthread_t threads[NUM_THREADS];
         int status;
-        for (int i=0; i < NUM_THREADS; i++) {
-                status = pthread_create(threads + i, NULL, new_thread, (void *)i);
+        for (long t=0; t < NUM_THREADS; t++) {
+                status = pthread_create(threads + t, NULL, new_thread, (void *)t);
                 if (status != 0){
                         printf("Error; return code from pthread_create() is %d\n", status);
                         exit(-1);
                 }
+        }
         return 0;
 }
-
