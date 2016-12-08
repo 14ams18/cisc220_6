44470370d721e3687d1266276138969773778a09
14ams18 <abigael.schonewille@hotmail.com>
2016-12-07 19:50:24 -0500

    Works!

--- Question1.c
+++ Question1.c
@@ -3,8 +3,6 @@
 #include<pthread.h>
 #include<unistd.h>
 
-#undef RAND_MAX
-#define RAND_MAX 100
 #undef NUM_THREADS
 #define NUM_THREADS 6
 
@@ -14,20 +12,28 @@ void *new_thread (void *threadid) {
         int lane = (int)threadid -1;
         do {
                 if (lane == 0) {  //draw thread
-                        printf("Welcome to the CISC220 Racing Arena! /nHit Enter to move.");
+                        system("clear");
+                        printf("Welcome to the CISC220 Racing Arena! \nHit Enter to move.\n");
                         for (int i=0; i < 5; i++) {
-                                for (int j=0; j<prog[i]; j++){
-                                        putchar('~');
+                                if (prog[i] < 40) {
+                                        for (int j=0; j<prog[i]; j++){
+                                                putchar('~');
+                                        }
+                                        printf("|->");
+                                        int invprog = 40 - prog[i];
+                                        for (int k=0; k<invprog; k++){
+                                                putchar(' ');
+                                        }
+                                        printf("# Lane %d #\n", i+1);
                                 }
-                                printf("|->");
-                                int invprog = 40 - prog[i];
-                                for (int k=0; k<invprog; k++){
-                                        putchar(' ');
+                                else {
+                                        for (int j=0; j<40; j++){
+                                                putchar('~');
+                                        }
+                                        printf("|-># Lane %d #\n", i+1);
                                 }
-                                printf("# Lane %d #\n", i+1);
                         }
                         sleep(20);
-                        system("clear");
                 }
                 else if (lane == 1) { // Player thead
                         char move = getchar();
@@ -36,16 +42,36 @@ void *new_thread (void *threadid) {
                         }
                 }
                 else { //start the AI threads lane == 2,3,4,5
-                        prog[lane-1] += 1;
-                        int srand = rand();
+                        int srand = rand() % 10;//decreased to 10 instead of 100 to make game harder
                         sleep(srand);
+                        prog[lane-1] += 1;
                 }
-        } while (prog[0] != 40 || prog[1] != 40 || prog[2] != 40 || prog[3] != 40 || prog[4] != 40);
+        } while (prog[0] < 40 && prog[1] < 40 && prog[2] < 40 && prog[3] < 40 && prog[4] < 40);
         if (lane == 0) { //draw thread
+                 system("clear");
+                        printf("Welcome to the CISC220 Racing Arena! \nHit Enter to move.\n");
+                        for (int i=0; i < 5; i++) {
+                                if (prog[i] < 40) {
+                                        for (int j=0; j<prog[i]; j++){
+                                                putchar('~');
+                                        }
+                                        printf("|->");
+                                        int invprog = 40 - prog[i];
+                                        for (int k=0; k<invprog; k++){
+                                                putchar(' ');
+                                        }
+                                        printf("# Lane %d #\n", i+1);
+                                }
+                                else {
+                                        for (int j=0; j<40; j++){
+                                                putchar('~');
+                                        }
+                                        printf("|-># Lane %d #\n", i+1);
+                                }
+                        }
                 for (int i=0; i < 5; i++) {
                         if (prog[i] == 40) {
                                 printf("Player %d WINS\n", i+1);
-
                         }
                 }
         }
@@ -55,12 +81,17 @@ void *new_thread (void *threadid) {
 int main(){
         pthread_t threads[NUM_THREADS];
         int status;
-        for (long t=0; t < NUM_THREADS; t++) {
+        long t;
+        for (t=1; t < NUM_THREADS+1; t++) {
                 status = pthread_create(threads + t, NULL, new_thread, (void *)t);
                 if (status != 0){
                         printf("Error; return code from pthread_create() is %d\n", status);
                         exit(-1);
                 }
         }
+        for (t = 0; t < NUM_THREADS; t++) {
+                pthread_join(threads[t], NULL);
+        }
+        pthread_exit(NULL);
         return 0;
 }
